f535a25b98ed68815aad3cccc4177681df1079df
Istvan Szukacs <istvan@datadeft.eu>
2021-12-01 19:53:59 +0100

    Simplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Alignment, Button, Navbar } from '@blueprintjs/core'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App/App'
 
 import './index.css'
+
+class NavBarTop extends React.PureComponent {
+  public render() {
+    return (
+      <Navbar>
+        <Navbar.Group align={Alignment.RIGHT}>
+          <Navbar.Heading>Depoxy</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className='bp4-minimal' icon='home' text='Home' />
+          <Navbar.Divider />
+          <Button className='bp4-minimal' icon='user' text='Profile' />
+        </Navbar.Group>
+      </Navbar>
+    )
+  }
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <NavBarTop />
+      <div className='App'>
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
